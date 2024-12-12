5e11a779ce926935e71d689dad60d92f1b20422f
AJ Javadi <amirjavadi25@gmail.com>
2024-12-12 07:09:14 -0600

    migrations looking good.

--- database/migrations/2024_12_12_125128_add_two_factor_columns_to_users_table.php
+++ database/migrations/2024_12_12_125128_add_two_factor_columns_to_users_table.php
@@ -5,26 +5,35 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 use Laravel\Fortify\Fortify;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->text('two_factor_secret')
-                ->after('password')
-                ->nullable();
+            if (!Schema::hasColumn('users', 'two_factor_secret')) {
 
-            $table->text('two_factor_recovery_codes')
-                ->after('two_factor_secret')
-                ->nullable();
-
-            if (Fortify::confirmsTwoFactorAuthentication()) {
-                $table->timestamp('two_factor_confirmed_at')
-                    ->after('two_factor_recovery_codes')
+                $table->text('two_factor_secret')
+                    ->after('password')
                     ->nullable();
+            }
+
+            if (!Schema::hasColumn('users', 'two_factor_recovery_codes')) {
+
+                $table->text('two_factor_recovery_codes')
+                    ->after('two_factor_secret')
+                    ->nullable();
+
+            }
+
+            if (!Schema::hasColumn('users', 'two_factor_confirmed_at')) {
+
+                if (Fortify::confirmsTwoFactorAuthentication()) {
+                    $table->timestamp('two_factor_confirmed_at')
+                        ->after('two_factor_recovery_codes')
+                        ->nullable();
+                }
             }
         });
     }
